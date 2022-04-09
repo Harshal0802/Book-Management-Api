5022ac1e2f306bd1f1939a5336b80e78a6a52ff6
Harshal <harshrathod1111@gmail.com>
2022-04-09 22:58:21 +0530

    fix model document issue

--- database/publication.js
+++ database/publication.js
@@ -8,6 +8,6 @@ const PublicationSchema = mongoose.Schema({
 });
 
 //create a publication schema 
-const PublicationModel = mongoose.model(PublicationSchema);
+const PublicationModel = mongoose.model("publications",PublicationSchema);
 
 module.exports = PublicationModel;
